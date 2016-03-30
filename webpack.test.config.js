af9a7f773b63091fd59ca3a3030cbd891ad1cd24
Leticia Figueira <jlcfigueira@gmail.com>
2016-03-30 18:16:22 -0300

    Adds babel-preset-react and firefox launcher for Travis

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,15 +1,15 @@
 var context = require.context('./app/scripts/tests', true, /Test\.(js|jsx)$/);
 
-beforeEach(() => {
+beforeEach(function() {
   sandbox = sinon.sandbox.create()
   requests = []
   xhr = sinon.useFakeXMLHttpRequest()
   xhr.onCreate = function (req) { requests.push(req); }
-})
+});
 
-afterEach(() => {
+afterEach(function() {
   xhr.restore()
   sandbox.restore()
-})
+});
 
 context.keys().forEach(context);
