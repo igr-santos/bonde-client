c2a1d8a031516454463d7c0de460cfe964949e73
Igor Santos <igr.exe@gmail.com>
2017-01-26 15:04:13 -0200

    Refactor button form to reuse component

--- client/components/forms/index.js
+++ client/components/forms/index.js
@@ -1,3 +1,3 @@
 export { default as Form } from './form'
 export { default as Input } from './input'
-export { default as SubmitButton } from './submit-button'
+export { default as Button } from './button'
