794f9316a9ad3f5b3747bec98a770f35fdb26608
Igor Santos <igr.exe@gmail.com>
2017-02-08 18:39:32 -0200

    Fix tests to render connected components like children

--- client/mobilizations/reducers/index.js
+++ client/mobilizations/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
-
+import widgetsReducer from '~client/mobrender/reducers/widgets'
 // Children module dependencies
 import templates from '../templates/reducers'
-
 // Current module dependencies
 import list from './list'
 
 export default combineReducers({
   list,
-  templates
+  templates,
+  widgets: widgetsReducer
 })
