ceecdcdbabda9a47dba2ea48f93c1c6074298e24
Igor Santos <igr.exe@gmail.com>
2017-03-13 11:16:58 -0300

    Fix layout for background component/container

--- routes/logged-in/background/container.js
+++ routes/logged-in/background/container.js
@@ -3,10 +3,15 @@ import React, { PropTypes } from 'react'
 import { Background } from '~client/components/layout'
 import { ZendeskWidget } from '~components/external-services'
 
+let bgImage
+if (process.env.BROWSER) {
+  bgImage = require('~client/images/bg-login.png')
+}
+
 const BackgroundContainer = ({ children }) => (
   <div>
     <ZendeskWidget />
-    <Background image={process.env.BROWSER ? require('~client/images/bg-login.png') : ''}>
+    <Background image={bgImage}>
       {children}
     </Background>
   </div>
